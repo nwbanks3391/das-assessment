a845af582ef56a46d45a38f312caea4bc8e77299
Nick <nwbanks3391@users.noreply.github.com>
2020-08-05 13:40:10 -0700

    Styling, error handling & optimization

--- frontend/coin-counts/src/components/ValueDisplay.js
+++ frontend/coin-counts/src/components/ValueDisplay.js
@@ -1,15 +1,44 @@
 import React from 'react';
+import { Table } from 'semantic-ui-react'
 
 const ValueDisplay = (props) => {
     return (
         <React.Fragment>
-            <p>COINS:</p>
-            <p>Silver Dollars: {props.data.silverDollar}</p>
-            <p>Half Dollars: {props.data.halfDollar}</p>
-            <p>Quarters: {props.data.quarter}</p>
-            <p>Dimes: {props.data.dime}</p>
-            <p>Nickels: {props.data.nickel}</p>
-            <p>Pennies: {props.data.penny}</p>
+            <p>Amount: ${props.amount}</p>
+            <Table celled>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>Coin Value</Table.HeaderCell>
+                        <Table.HeaderCell>Amount</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                    <Table.Row>
+                        <Table.Cell>Silver Dollars</Table.Cell>
+                        <Table.Cell>{props.data.silverDollar}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell>Half Dollars</Table.Cell>
+                        <Table.Cell>{props.data.halfDollar}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell>Quarters</Table.Cell>
+                        <Table.Cell>{props.data.quarter}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell>Dimes</Table.Cell>
+                        <Table.Cell>{props.data.dime}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell>Nickels</Table.Cell>
+                        <Table.Cell>{props.data.nickel}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell>Pennies</Table.Cell>
+                        <Table.Cell>{props.data.penny}</Table.Cell>
+                    </Table.Row>
+                </Table.Body>
+            </Table>
         </React.Fragment>
     );
 };
--- frontend/coin-counts/src/pages/CoinCounts.js
+++ frontend/coin-counts/src/pages/CoinCounts.js
@@ -1,7 +1,9 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, } from 'react';
 import axios from 'axios'
-import { Container, Header } from 'semantic-ui-react'
+import { Container, Header  } from 'semantic-ui-react'
 import styled from 'styled-components'
+import Spinner from 'react-spinkit'
+
 
 import AmountField from '../components/AmountField';
 import ValueDisplay from '../components/ValueDisplay';
@@ -24,26 +26,37 @@ const StyledContainer = styled(Container)`
     box-shadow: 5px 5px 8px #888888;
 `
 
+const ErrorText = styled.h3`
+    color: red;
+    font-weight: bold;
+`
+
 const CoinCounts = () => {
     const [amount, setAmount] = useState('');
     const [counts, setCounts] = useState({});
     const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
-    let typingTimer = null;
+    const [error, setError] = useState('');
 
     const handleAmountChange = (e) => {
+        if(counts.SilverDollar !== null) {
+            setCounts({})
+        }
         const val = e.target.value;
         setAmount(val)
     }
 
     const handleRequest = () => {
+        setError('')
         const amt = parseFloat((Math.floor(parseFloat(amount) * 100) / 100).toFixed(2)) //set this way to prevent rounding
+        if(typeof(amt) !== 'number'){
+            setError('Please enter a valid number')
+            return
+        }
+        setAmount(amt)
         const data = {
             Value: amt
         }
-        console.log(data)
         setLoading(true)
-        setError(false)
         // axios.post('https://localhost:5001/coincount', data).then((response) => {
         axios({
             method: 'post',
@@ -58,7 +71,7 @@ const CoinCounts = () => {
         }).catch((error)=> {
             console.log(error)
             setLoading(false)
-            setError(true)
+            setError('An error has occurred. Please refresh the page and try again.')
         })
     }
 
@@ -69,12 +82,17 @@ const CoinCounts = () => {
                     Coin Count Optimizer
                 </Header>
                 <p>Enter an amount of Dollars and Cents:</p>
-                <br/>
                 <AmountField onChange={handleAmountChange} onSubmit={handleRequest}/>
                 {//separate this into its own component for structure
                     counts.silverDollar ? 
-                        <ValueDisplay data={counts}/>
+                        <ValueDisplay data={counts} amount={amount}/>
                     : ''
+                }
+                {
+                    error === '' ? '' : <ErrorText>{error}</ErrorText>
+                }
+                {
+                    loading === true ? <Spinner name='three-bounce'/> : ''
                 }
             </StyledContainer>
         </Background>
